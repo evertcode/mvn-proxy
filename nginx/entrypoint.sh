01a3839dd2071dee5953de1b77062fb0dcec0a17
Hebert Hernandez <hebert.hernandez7@gmail.com>
2025-04-15 16:44:55 -0600

    Replace envsubst with sed and simplify Dockerfile dependencies

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 # Genera credenciales en Base64
-export AUTH_CREDENTIALS=$(echo -n "${REPO_USER}:${REPO_PASSWORD}" | base64)
+AUTH_CREDENTIALS=$(echo -n "${REPO_USER}:${REPO_PASSWORD}" | base64)
 
-# Reemplaza variables en la plantilla de NGINX
-envsubst '${REPO_URL},${REPO_HOST},${AUTH_CREDENTIALS}' < /etc/nginx/templates/maven-proxy.conf.template > /etc/nginx/conf.d/default.conf
+# Reemplaza variables en el archivo de configuración usando sed
+sed -i \
+  -e "s|REPO_URL_PLACEHOLDER|${REPO_URL}|g" \
+  -e "s|REPO_HOST_PLACEHOLDER|${REPO_HOST}|g" \
+  -e "s|AUTH_CREDENTIALS_PLACEHOLDER|${AUTH_CREDENTIALS}|g" \
+  /etc/nginx/conf.d/default.conf
 
 # Inicia NGINX
 exec "$@"
